c4d015e474432a5e65fa55c6e86a09553762d614
window-exe <75521350+window-exe@users.noreply.github.com>
2023-04-17 20:14:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -51,19 +51,19 @@ async function checkweather(city) {
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
     if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "/img/images/clouds.png"
+      weatherIcon.src = "/TBA/img/images/clouds.png"
     }
     else if (data.weather[0].main == "Clear") {
-      weatherIcon.src = "/img/images/clear.png"
+      weatherIcon.src = "/TBA/img/images/clear.png"
     }
     else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = "/img/images/rain.png"
+      weatherIcon.src = "/TBA/img/images/rain.png"
     }
     else if (data.weather[0].main == "Drizzle") {
-      weatherIcon.src = "/img/images/drizzle.png"
+      weatherIcon.src = "/TBA/img/images/drizzle.png"
     }
     else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = "/img/images/mist.png"
+      weatherIcon.src = "/TBA/img/images/mist.png"
     }
 
     document.querySelector(".weather2").style.display = "block"
